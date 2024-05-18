2d7f79e739778f9eaedf9b623017a12bb5a29356
ken-morel <engonken8@gmail.com>
2024-05-18 06:24:08 +0100

    learning...

--- src/maps/jumpist.tsx
+++ src/maps/jumpist.tsx
@@ -1,2 +1,24 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.5" name="jumpist" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
+<tileset version="1.2" tiledversion="1.2.5" name="jumpist" tilewidth="128" tileheight="128" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="7">
+  <image width="128" height="128" source="../tilesets/pac-0.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="../tilesets/pac-1.png"/>
+  <animation>
+   <frame tileid="7" duration="210"/>
+   <frame tileid="8" duration="210"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="../tilesets/pac-2.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="../tilesets/pac-3.png"/>
+  <animation>
+   <frame tileid="9" duration="250"/>
+   <frame tileid="10" duration="250"/>
+  </animation>
+ </tile>
+</tileset>
